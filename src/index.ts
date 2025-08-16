27e3810dd2a941ddfbed28d41bccb92420376f7e
subhojit dutta <dsubhojit063@gmail.com>
2025-08-16 12:30:34 +0530

    t

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,8 @@ import taskRoutes from "./routes/task.route";
 const app = express();
 const BASE_PATH = config.BASE_PATH;
 
+app.set("trust proxy", 1); // trust first proxy
+
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
