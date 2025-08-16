dd9936fb9ecd18e2e0fafb73170eb51bd7e8bab3
subhojit dutta <dsubhojit063@gmail.com>
2025-08-16 10:48:30 +0530

    t

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -6,7 +6,7 @@ import { getCurrentUserService } from "../services/user.service";
 export const getCurrentUserController = asyncHandler(
   async (req: Request, res: Response) => {
     const userId = req.user?._id;
-
+    console.log({ userId });
     const { user } = await getCurrentUserService(userId);
 
     return res.status(HTTPSTATUS.OK).json({
